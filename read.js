01ef61553798a1b704b7b011b863e0dd71a20b8e
robocopkaka <robocopkaka@gmail.com>
2016-08-22 17:05:42 +0100

    get and posts work well now, tests to follow

--- read.js
+++ read.js
@@ -29,17 +29,18 @@ rl.question('Would you like to view your medium details? ', (answer) => {
 	});
 
 
-	request({
-	    url: 'https://api.medium.com/v1/users/{{1b55ff57a2f0ea9780657f5da3ffd656c33480f4206c6a978846ae9c6a87ae7f6}}/posts', //URL to hit
-	    method: 'POST',
-	    headers: {
-	         'Authorization': 'Bearer 2e5cdee128202fa37fd4f37ba3308a3ff277ac5d4ceb256df2e151b20a14f3278'
-	    },
-	    body: {"title": "Trying out Medium's API",
-		"contentFormat": "HTML",
-		"contentFormat": "<p><h1> Medium API, to be deleted after </h1></p>",
+	request.post({
+	    url: 'https://api.medium.com/v1/users/1b55ff57a2f0ea9780657f5da3ffd656c33480f4206c6a978846ae9c6a87ae7f6/posts', //URL to hit
+	    json: {
+	    "title": "Trying out Medium's API",
+		"contentFormat": "html",
+		"content": "<p><h1> Medium API, to be deleted after </h1></p>",
 		"tags": ["api"],
     	"publishStatus": "unlisted"},
+	    headers: {
+	         'Authorization': 'Bearer 2e5cdee128202fa37fd4f37ba3308a3ff277ac5d4ceb256df2e151b20a14f3278',
+			'Accept-Charset': 'utf-8'
+	    }
 	}, function(error, response, body){
 	    if(error) {
 	        console.log(error);
