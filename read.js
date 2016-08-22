322adf92522b6f042ee791d6e7bdee3c139ce414
robocopkaka <robocopkaka@gmail.com>
2016-08-22 14:42:42 +0100

    with get and post

--- read.js
+++ read.js
@@ -5,13 +5,49 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
-rl.question('Would you like to sign in to Medium? ', (answer) => {
+rl.question('Would you like to view your medium details? ', (answer) => {
   // TODO: Log the answer in a database
   if(answer == "yes") {
-  	request.get("https://www.github.com").auth('robocopkaka', 'Jessica1',  false)
-  	
-  }
-  console.log('Thank you for your valuable feedback:', answer);
+  	console.log(`Received: ${answer}`);
+  	var mediumWrapi = require('medium-wrapi');
+ 
+	var client = new mediumWrapi("v1", "2e5cdee128202fa37fd4f37ba3308a3ff277ac5d4ceb256df2e151b20a14f3278"
+	);
 
+	request({
+	    url: 'https://api.medium.com/v1/me', //URL to hit
+	    method: 'GET', //Specify the method
+	    headers: { //We can define headers too
+	        'Authorization': 'Bearer 2e5cdee128202fa37fd4f37ba3308a3ff277ac5d4ceb256df2e151b20a14f3278'
+	    }
+	}, function(error, response, body){
+	    if(error) {
+	        console.log(error);
+	    } else {
+	        console.log(response.statusCode, body);
+	    }
+	});
+
+
+	request({
+	    url: 'https://api.medium.com/v1/users/{{1b55ff57a2f0ea9780657f5da3ffd656c33480f4206c6a978846ae9c6a87ae7f6}}/posts', //URL to hit
+	    method: 'POST',
+	    headers: {
+	         'Authorization': 'Bearer 2e5cdee128202fa37fd4f37ba3308a3ff277ac5d4ceb256df2e151b20a14f3278'
+	    },
+	    body: {"title": "Trying out Medium's API",
+		"contentFormat": "HTML",
+		"contentFormat": "<p><h1> Medium API, to be deleted after </h1></p>",
+		"tags": ["api"],
+    	"publishStatus": "unlisted"},
+	}, function(error, response, body){
+	    if(error) {
+	        console.log(error);
+	    } else {
+	        console.log(response.statusCode, body);
+	    }
+	});
+  console.log('Thank you for your valuable feedback:', answer);
+}
   rl.close();
 });
